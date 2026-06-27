5a3223c723cf2ad7f38b5a95e704b2384c545b5d
Arvind-Chandak <arvindchandak061@gmail.com>
2026-06-27 13:45:05 +0530

    fix: force IPv4 DNS resolution for Gmail OAuth2 SMTP on Render

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
